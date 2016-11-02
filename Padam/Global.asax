a709a42d66f69678a991894ff311e15dd23087c5
Sunil Suriamurthy <sunil.f987@gmail.com>
2016-11-02 17:07:47 +0530

    inital check-ins

---
+++ Padam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Padam.MvcApplication" Language="C#" %>
---
+++ Padam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Padam.Startup))]
+namespace Padam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
