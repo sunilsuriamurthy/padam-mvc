250eb562678382fdb884e6342b0a347a57127b85
Sunil Suriamurthy <sunil.f987@gmail.com>
2016-11-05 12:13:37 +0530

    Commit whatever skeletal code done till now

---
+++ Padam/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Padam.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+    }
+}
